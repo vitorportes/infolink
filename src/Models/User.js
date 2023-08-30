a764a7ad5c62f2a9aeea3ad31ae09f447e2571b8
vitorportes <vitorportnoy@gmail.com>
2023-08-30 11:10:28 -0300

    added password encryption

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -18,6 +19,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+        select: false,
     },
     avatar: {
         type: String,
@@ -27,6 +29,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+});
+
+userSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
